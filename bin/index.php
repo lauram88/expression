05e3ff54bab9245af75969ffe9e3d93b65afff00
Laura Magureanu <laura88_m@yahoo.com>
2016-04-15 15:35:21 +0300

    Epression first version

--- bin/index.php
+++ bin/index.php
@@ -1,5 +1,18 @@
 <?php
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../src/calculate_expression.php';
 
-echo 'Front controller!' . PHP_EOL;
+use Symfony\Component\Console\Application;
+use Laura\Expressions\Command\GreetCommand;
+use Laura\Expressions\Command\CalculateExpressions;
+
+$application = new Application();
+$application->add(new CalculateExpressions());
+$application->run();
+
+//$calc_expr = new Calculate_expression();
+//$espr = "F&((T&F)|T&(F|T))&(F|T)";
+//$calc_expr->calc_expr($espr);
+
+//echo 'Front controller!' . PHP_EOL;
---
+++ src/Command/CalculateExpressions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laura.magureanu
+ * Date: 14.04.2016
+ * Time: 17:31
+ */
+
+namespace Laura\Expressions\Command;
+
+
+class CalculateExpressions
+{
+
+}
