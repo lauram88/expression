8fa2d0f4a9ab4dc3fd4bf483f49cee6866e9c857
Laura Magureanu <laura88_m@yahoo.com>
2016-04-25 12:54:36 +0300

    Version 2.2

--- bin/index.php
+++ bin/index.php
@@ -1,7 +1,6 @@
 <?php
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
-$loader = require __DIR__ . '/../src/calculate_expression.php';
 
 use Symfony\Component\Console\Application;
 use Laura\Expressions\Command\GreetCommand;
@@ -10,9 +9,3 @@ use Laura\Expressions\Command\CalculateExpressions;
 $application = new Application();
 $application->add(new CalculateExpressions());
 $application->run(); 
-
-//$calc_expr = new Calculate_expression();
-//$espr = "F&((T&F)|T&(F|T))&(F|T)";
-//$calc_expr->calc_expr($espr);
-
-//echo 'Front controller!' . PHP_EOL;
--- src/Command/CalculateExpressions.php
+++ src/Command/CalculateExpressions.php
@@ -16,7 +16,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CalculateExpressions extends Command
 {
-    var $bool_values = array("T"=>true, "F"=>false);
+    private $boolValues = array("T"=>true, "F"=>false);
 
     protected function configure()
     {
@@ -35,11 +35,101 @@ class CalculateExpressions extends Command
     {
         $expression = $input->getArgument('expression');
 
-        $result = $this->CalcExpr($expression);
+        $expression = $this->fixForRpn($expression);
+
+        $result = $this->reversePolishNotation($expression);
+
+        if ($result=="") {
+            $result = "Please verify if the expression you asked for is correct ( Eg: (F&(T&F))|T&(F|T)&F|T  ; T|F&T&(F|T&(T|F)) )";
+        }
 
         $text = 'Final Answer: '.$result ;
 
         $output->writeln($text);
+    }
+
+
+    function fixForRpn($expression){
+        $newExpression = array();
+        $string = "";
+        $operandsList = array();
+        $values = array('T'=>true, 'F'=>false);
+        $length = strlen($expression);
+        $i = 0;
+        $push = true;
+        while ($i<$length) {
+            if ($expression[$i]=="(") {
+                $push = false;
+            }elseif ($expression[$i]==")") {
+                $push = true;
+                $val = array_pop($operandsList);
+                array_push($newExpression, $val);
+                $string.=$val;
+                if (count($operandsList)>0) {
+                    $val = array_pop($operandsList);
+                    array_push($newExpression, $val);
+                    $string.=$val;
+                }
+            }elseif (in_array($expression[$i], array("&", "|"))) {
+                array_push($operandsList, $expression[$i]);
+            }elseif (in_array($expression[$i], array_keys($values))) {
+                array_push($newExpression, $expression[$i]);
+                $string.=$expression[$i];
+                if ($push) {
+                    $val = array_pop($operandsList);
+                    if ($val!="") {
+                        array_push($newExpression, $val);
+                        $string.=$val;
+                    }
+                }
+            }
+            $i++;
+        }
+        if (count($operandsList)>0) {
+            $val = array_pop($operandsList);
+            array_push($newExpression, $val);
+            $string.=$val;
+        }
+        return $newExpression;
+    }
+
+
+    function reversePolishNotation($params) {
+        $values = array('T'=>true, 'F'=>false);
+        $count = sizeof($params);
+        $result = null;
+        $valueToAdd = null;
+        $numeric = array();
+
+        for($i = 0; $i < $count; $i++) {
+            if ( in_array( $params[$i], array_keys($values) ) ) {
+                $numeric[] = $params[$i];
+            } else {
+                if (count($numeric)>1){
+                    $pop1 = array_pop($numeric);
+                    $pop2 = array_pop($numeric);
+                    switch ($params[$i]) {
+                        case "&":
+                            $result = $values[$pop1] && $values[$pop2];
+                            break;
+                        case "|":
+                            $result = $values[$pop1] || $values[$pop2];
+                            break;
+                    }
+                }
+                if ($result) {
+                    $valueToAdd = "T";
+                }else{
+                    $valueToAdd = "F";
+                }
+                array_push($numeric, $valueToAdd);
+            }
+        }
+
+        if ($count==1) {
+            $valueToAdd = array_pop($numeric);
+        }
+        return $valueToAdd;
     }
 
     private function CalcExpr($expr){
@@ -49,7 +139,7 @@ class CalculateExpressions extends Command
         preg_match($pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
 
         /*cat timp mai avem paraneze rulam recursive match*/
-        while (is_string($expr) && (strlen($expr)>1)){
+        while (is_string($expr) && (strlen($expr)>1)) {
 
             $expr = $this->RecursiveMatch($expr);
 
@@ -57,6 +147,7 @@ class CalculateExpressions extends Command
 
         return $expr;
     }
+
     private function RecursiveMatch($expr){
 
         /*cat timp noi avem paranteza in paranteza vom merge recursiv ruland tot aceasta functie pe subparanteza gasita*/
@@ -66,51 +157,50 @@ class CalculateExpressions extends Command
 
         //print_r($matches);
 
-        if (count($matches)>0){
+        if (count($matches)>0) {
             /*in substringul nostru mai avem paranteze*/
             $return_val = $this->RecursiveMatch($matches[1][0]);
 
             /*inlocuim resultatul cu informatia din $expr*/
-            if (!is_bool($return_val)){
+            if (!is_bool($return_val)) {
 
-                if ($return_val=="T"){
+                if ($return_val=="T") {
 
                     $return_val = true;
 
-                }else{
+                } else {
 
                     $return_val = false;
 
                 }
             }
-            if ($return_val ){
+            if ($return_val ) {
 
                 $bool_vall= str_replace("(".$matches[1][0].")", "T", $expr );
 
-            }else{
+            } else {
 
                 $bool_vall = str_replace("(".$matches[1][0].")", "F", $expr );
 
             }
             print_r("valoare de dupa str_replace:");
             var_dump($bool_vall);
-        }
-        else{
+        } else {
             /*nu mai avem paranteze*/
             $bool_vall_result = $this->ValuesWithoutBrackets($expr);
 
-            if (is_bool($bool_vall_result)){
+            if (is_bool($bool_vall_result)) {
 
-                if ($bool_vall_result){
+                if ($bool_vall_result) {
 
                     $bool_vall="T";
 
-                }else{
+                } else {
 
                     $bool_vall="F";
 
                 }
-            }else{
+            } else {
 
                 $bool_vall = $bool_vall_result;
 
@@ -121,7 +211,8 @@ class CalculateExpressions extends Command
 
     }
 
-    private function ValuesWithoutBrackets($expr){
+    private function ValuesWithoutBrackets($expr)
+    {
         /*putem avea o epresie de genul : T&F|F|F - vom mlerge pe fiecare caracter din aceasta expresie si vom calcula pe rand */
         $return_val = "";
 
@@ -131,22 +222,22 @@ class CalculateExpressions extends Command
 
         for ($i = 0; $i < $length; $i++) {
 
-            if ( !in_array($expr[$i], array('|', '&') ) ){
+            if ( !in_array($expr[$i], array('|', '&') ) ) {
 
-                if ($return_val ==""){
+                if ($return_val =="") {
 
                     $return_val = $expr[$i];
 
-                }else{
+                } else {
 
                     /*trebuie sa avem prima valoare, operatorul , si urmatoarea valoare - toate necesare pentru calcul*-*/
                     $return_val = $this->CalculateOneStatement($return_val , $stored_operator, $expr[$i]);
 
-                    if ($return_val){
+                    if ($return_val) {
 
                         $return_val = "T";
 
-                    }else{
+                    } else {
 
                         $return_val = "F";
 
@@ -155,8 +246,7 @@ class CalculateExpressions extends Command
                     $stored_operator = "";
 
                 }
-            }
-            else{
+            } else {
 
                 $stored_operator  = $expr[$i];
 
@@ -165,20 +255,21 @@ class CalculateExpressions extends Command
         return $return_val;
     }
 
-    private function CalculateOneStatement($first_value, $operator, $second_value){
+    private function CalculateOneStatement($first_value, $operator, $second_value)
+    {
 
         /* aceasta calculeaza doar expresiile cu doua valori*/
         switch ($operator) {
 
             case "|":
 
-                return ($this->bool_values[$first_value] || $this->bool_values[$second_value]);
+                return ($this->boolValues[$first_value] || $this->boolValues[$second_value]);
 
                 break;
 
             case "&":
 
-                return ($this->bool_values[$first_value] && $this->bool_values[$second_value]);
+                return ($this->boolValues[$first_value] && $this->boolValues[$second_value]);
 
                 break;
 
@@ -188,130 +279,6 @@ class CalculateExpressions extends Command
 
                 return true;
         }
-    }
-
-
-    function rpn($params) {
-        $pattern = '#\(((?>[^()]+)|(?R))*\)#x';
-        preg_match_all($pattern, $params, $matches, PREG_OFFSET_CAPTURE);
-
-        /*in matches avem pozitiile de la care incep parantezele*/
-        print_r($matches);
-
-        /*incercam sa creem un array care sa contina */
-        $params = explode(' ', $params);
-
-        $count = sizeof($params);
-
-        $result = null;
-
-        $numeric = array();
-
-        for($i = 0; $i < $count; $i++) {
-
-            if (is_numeric($params[$i])) {
-
-                $numeric[] = $params[$i];
-
-            } else {
-
-                switch ($params[$i]) {
-                    case "+":
-                        $result = array_pop($numeric) + array_pop($numeric);
-                        break;
-                    case "-":
-                        $result = array_pop($numeric) - array_pop($numeric);
-                        break;
-                    case "*":
-                        $result = array_pop($numeric) * array_pop($numeric);
-                        break;
-                    case "/":
-                        $result = array_pop($numeric) / array_pop($numeric);
-                        break;
-                }
-                array_push($numeric, $result);
-            }
-        }
-        return $result;
-    }
-
-    function rpn_modified($params) {
-
-        $params = explode(' ', $params);
-
-        $count = sizeof($params);
-
-        $result = null;
-
-        $numeric = array();
-
-        for($i = 0; $i < $count; $i++) {
-
-            if (in_array($params[$i], $this->bool_values )) {
-
-                $numeric[] = $params[$i];
-
-            } else {
-
-                switch ($params[$i]) {
-
-                    case "|":
-                        $result = $this->bool_values[array_pop($numeric)] || $this->bool_values[array_pop($numeric)];
-                        break;
-                    case "&":
-                        $result = $this->bool_values[array_pop($numeric)] && $this->bool_values[array_pop($numeric)];
-                        break;
-                    case "(":
-                        //$result = array_pop($numeric) * array_pop($numeric);
-                        break;
-                    case ")":
-                        //$result = array_pop($numeric) / array_pop($numeric);function rpn_modified($params) {
-
-                        $params = explode(' ', $params);
-
-                        $count = sizeof($params);
-
-                        $result = null;
-
-                        $numeric = array();
-
-                        for($i = 0; $i < $count; $i++) {
-
-                            if (in_array($params[$i], $this->bool_values )) {
-
-                                $numeric[] = $params[$i];
-
-                            } else {
-
-                                switch ($params[$i]) {
-
-                                    case "|":
-                                        $result = $this->bool_values[array_pop($numeric)] || $this->bool_values[array_pop($numeric)];
-                                        break;
-                                    case "&":
-                                        $result = $this->bool_values[array_pop($numeric)] && $this->bool_values[array_pop($numeric)];
-                                        break;
-                                    case "(":
-                                        //$result = array_pop($numeric) * array_pop($numeric);
-                                        break;
-                                    case ")":
-                                        //$result = array_pop($numeric) / array_pop($numeric);
-                                        break;
-                                }
-                                array_push($numeric, $result);
-                            }
-                        }
-                        return $result;
-                }
-                break;
-            }
-            array_push($numeric, $result);
-            echo "<pre>";
-            print_r($result);
-            echo "</pre>";
-        }
-
-        return $result;
     }
 
 }
